c8cefbeac0e2081de6744df76b92ed7e625a955e
Digital Fusion <df1317@gmail.com>
2023-02-07 18:30:42 -0500

    code builds, auto not fully implemented

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -6,7 +6,7 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
+// import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
@@ -29,6 +29,12 @@ public class Drivetrain {
     private final WPI_VictorSPX frontRightMotor = new WPI_VictorSPX(2);
     private final WPI_VictorSPX backRightMotor = new WPI_VictorSPX(3);
 
+    public static final double kMaxSpeed = 0.5; // meters per second
+    public static final double kMaxAngularSpeed = 2 * Math.PI * 0.125; // one rotation per second
+    public static final double kTrackWidth = 0.381 * 2; // meters
+    public static final double kWheelRadius = 0.0508; // meters
+    public static final int kEncoderResolution = 4096;
+
     private final MotorControllerGroup leftMotorGroup = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
     private final MotorControllerGroup rightMotorGroup = new MotorControllerGroup(frontRightMotor, backRightMotor);
     private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
@@ -46,11 +52,11 @@ public class Drivetrain {
   
     // Gains are for example purposes only - must be determined for your own robot!
     private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(0.77217, 3.1527, 1.5513);
-  
+
     Gyro gyro = new Gyro();
 
     public Drivetrain() {
-        gyro.gyro.reset(); 
+        // make sure to reset gyro when we start auto and when robot init please!!!
         leftMotorGroup.setInverted(true);
         rightMotorGroup.setInverted(false);
         m_leftEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
@@ -62,7 +68,7 @@ public class Drivetrain {
     m_odometry =
         new DifferentialDriveOdometry(
           // maybe negate m_gyro.getAngle()? atleast for NavX apparently
-          Rotation2d.fromDegrees(m_gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+          Rotation2d.fromDegrees(gyro.gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
     }
     public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
         final double leftFeedforward = m_feedforward.calculate(speeds.leftMetersPerSecond);
@@ -72,8 +78,8 @@ public class Drivetrain {
             m_leftPIDController.calculate(m_leftEncoder.getRate(), speeds.leftMetersPerSecond);
         final double rightOutput =
             m_rightPIDController.calculate(m_rightEncoder.getRate(), speeds.rightMetersPerSecond);
-        m_leftGroup.setVoltage(leftOutput + leftFeedforward);
-        m_rightGroup.setVoltage(rightOutput + rightFeedforward);
+        leftMotorGroup.setVoltage(leftOutput + leftFeedforward);
+        rightMotorGroup.setVoltage(rightOutput + rightFeedforward);
       }
       public void autoDrive(double xSpeed, double rot) {
         var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
@@ -81,11 +87,11 @@ public class Drivetrain {
       }
       public void updateOdometry() {
         m_odometry.update(
-          Rotation2d.fromDegrees(m_gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+          Rotation2d.fromDegrees(gyro.gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
       }
       public void resetOdometry(Pose2d pose) {
         m_odometry.resetPosition(
-          Rotation2d.fromDegrees(m_gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), pose);
+          Rotation2d.fromDegrees(gyro.gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), pose);
       }
       public Pose2d getPose() {
         return m_odometry.getPoseMeters();
